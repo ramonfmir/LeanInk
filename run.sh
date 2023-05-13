df107297c74e14aa22654b5dd32ecd2b7a471242
Ramon Fernandez Mir <rfernandezmir@gmail.com>
2023-05-13 11:47:26 -0400

    Only extracting from modules with no leanInk file

--- run.sh
+++ run.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
-#TODO: Add "Algebra" 
-packages=("AlgebraicTopology" "Analysis" "CategoryTheory" "Combinatorics" "Computability" "Control" "Data" "Dynamics" "FieldTheory" "Geometry" "GroupTheory" "InformationTheory" "LinearAlgebra" "Logic" "MeasureTheory" "ModelTheory" "NumberTheory" "Order" "Probability" "RepresentationTheory" "RingTheory" "SetTheory" "Topology")
+packages=("Algebra" "AlgebraicTopology" "Analysis" "CategoryTheory" "Combinatorics" "Computability" "Control" "Data" "Dynamics" "FieldTheory" "Geometry" "GroupTheory" "InformationTheory" "LinearAlgebra" "Logic" "MeasureTheory" "ModelTheory" "NumberTheory" "Order" "Probability" "RepresentationTheory" "RingTheory" "SetTheory" "Topology")
 
 for pkg in "${packages[@]}"
 do
     mods=`find ./lake-packages/mathlib/Mathlib/$pkg -name "*.lean"`
-    ./build/bin/leanInk a $mods  --lake ./lake-packages/mathlib/lakefile.lean --x-enable-type-info 
+    for mod in $mods 
+    do 
+        modInk="${mod}.leanInk"
+        if test -f "$modInk"; then
+            echo "$modInk exists."
+        else
+            echo "$modInk does not exist."
+            ./build/bin/leanInk a $mod --lake ./lake-packages/mathlib/lakefile.lean --x-enable-type-info 
+        fi
+    done
 done
