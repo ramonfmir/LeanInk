09028a9dbf7a53894b7ca54d682ad1b19cece451
Ramon Fernandez Mir <rfernandezmir@gmail.com>
2023-05-23 19:32:04 +0100

    Count premises

--- count.sh
+++ count.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
 data=`find ./lake-packages/mathlib/Mathlib -name "*.leanInk"`
-count=0
+count_examples=0
+count_premises=0
 
 for d in $data 
 do
-    items=`jq length $d`
-    count=$((count+items))
+    examples=`jq length $d`
+    premises=`jq '.[] | .contents' $d | jq '.[]' | wc -l`
+    count_examples=$((count_examples + examples))
+    count_premises=$((count_premises + premises))
 done
 
-echo $count
+echo $count_examples
+echo $count_premises
